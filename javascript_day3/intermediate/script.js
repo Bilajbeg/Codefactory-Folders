7002d87e78c3486d258785343d851aaa8e1df16c
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-05 21:55:33 +0200

    works well now

---
+++ javascript_day3/intermediate/script.js
@@ -0,0 +1,19 @@
+for(let i=0; i<6; i++) {
+
+    let var_1 = '';
+    for(let j=0; j <= i; j++) {
+    var_1 = var_1 + '*';
+    }
+    console.log(var_1);
+    
+}
+
+for(let i=0; i<6; i++) {
+
+    let var_1 = '';
+    for(let j=0; j <= i; j++) {
+    var_1 = var_1 + '*';
+    }
+    document.write(var_1); 
+    document.write("<br>");
+}
