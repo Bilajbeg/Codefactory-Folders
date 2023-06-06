135685605f8b2d0dcb22c42fbac0f5e19f8827df
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-06 12:03:55 +0200

    intermediate folder with files created

---
+++ javascript_day4/intermediate/script_1.js
@@ -0,0 +1,29 @@
+// Array of Santa Claus image URLs
+const santaUrls = [
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png"
+  ];
+  
+  // Create the 3x3 matrix
+  const matrixDiv = document.getElementById("matrix");
+  
+  for (let i = 0; i < santaUrls.length; i++) {
+    const santaImg = document.createElement("img");
+    santaImg.src = santaUrls[i];
+    santaImg.alt = "Santa Claus";
+    santaImg.classList.add("santa-claus");
+  
+    santaImg.addEventListener("click", function() {
+      this.style.visibility = "hidden";
+    });
+  
+    matrixDiv.appendChild(santaImg);
+  }
+  
