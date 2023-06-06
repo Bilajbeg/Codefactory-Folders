da4107964724ba3b6fa6f16fed6a4fe78f0c84a5
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-06 14:23:30 +0200

    advanced exercise finished

--- javascript_day4/advanced/script.js
+++ javascript_day4/advanced/script.js
@@ -10,5 +10,18 @@ circle.addEventListener("mouseout", function(){
     document.getElementById("result").innerHTML = "You are outsite the circle";
 })
 
-
+// Function to handle the click and double-click events on the circle
+function handleCircleEvents() {
+    const circle = document.getElementById("circle");
   
+    circle.addEventListener("click", function() {
+      document.body.style.backgroundColor = "gray";
+    });
+  
+    circle.addEventListener("dblclick", function() {
+      document.body.style.backgroundColor = "blue";
+    });
+  }
+  
+  // Call the handleCircleEvents function when the DOM content is loaded
+  document.addEventListener("DOMContentLoaded", handleCircleEvents);
