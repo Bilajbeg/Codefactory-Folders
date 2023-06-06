2c8e20b6da29cf1ca45c1220ee1d2bed9f317924
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-06 10:50:44 +0200

    is

---
+++ javascript_day4/basic/script_1.js
@@ -0,0 +1,21 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Extract values from input fields
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const age = document.getElementById("age").value;
+  
+    // Display values in separate div
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `First Name: ${firstName}<br>
+                           Last Name: ${lastName}<br>
+                           Age: ${age}`;
+  
+    // Change text color based on name length
+    if (firstName.length > 5 || lastName.length > 5) {
+      outputDiv.style.color = "green";
+    } else {
+      outputDiv.style.color = "red";
+    }
+  });
