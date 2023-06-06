dd465812e7e2af39ffc9c0f18a0b6eb4a8845600
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-06 11:03:15 +0200

    basic ex_1 finished

--- javascript_day4/basic/script_1.js
+++ javascript_day4/basic/script_1.js
@@ -3,7 +3,7 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
   
     // Extract values from input fields
     const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
+    const lastName  = document.getElementById("lastName").value;
     const age = document.getElementById("age").value;
   
     // Display values in separate div
