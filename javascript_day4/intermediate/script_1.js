942268bdea5400b4d13e84f29e8f1adc33eae110
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-06 12:37:33 +0200

    made some changes with the pictures

--- javascript_day4/intermediate/script_1.js
+++ javascript_day4/intermediate/script_1.js
@@ -1,13 +1,13 @@
 // Array of Santa Claus image URLs
 const santaUrls = [
+  "https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878",
   "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878",
+  "https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878",
   "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878",
   "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878",
   "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png"
 ];
 
