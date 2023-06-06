fdef2182d4dfec36ac490ddcf52ee12914ad9245
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-06 12:26:40 +0200

    intermediate ex_1 finished

--- javascript_day4/intermediate/script_1.js
+++ javascript_day4/intermediate/script_1.js
@@ -1,29 +1,28 @@
 // Array of Santa Claus image URLs
 const santaUrls = [
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
-    "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png"
-  ];
-  
-  // Create the 3x3 matrix
-  const matrixDiv = document.getElementById("matrix");
-  
-  for (let i = 0; i < santaUrls.length; i++) {
-    const santaImg = document.createElement("img");
-    santaImg.src = santaUrls[i];
-    santaImg.alt = "Santa Claus";
-    santaImg.classList.add("santa-claus");
-  
-    santaImg.addEventListener("click", function() {
-      this.style.visibility = "hidden";
-    });
-  
-    matrixDiv.appendChild(santaImg);
-  }
-  
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png",
+  "https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png"
+];
+
+// Create the 3x3 grid
+const matrixDiv = document.getElementById("matrix");
+
+for (let i = 0; i < santaUrls.length; i++) {
+  const santaImg = document.createElement("img");
+  santaImg.src = santaUrls[i];
+  santaImg.alt = "Santa Claus";
+  santaImg.classList.add("santa-claus");
+
+  santaImg.addEventListener("click", function() {
+    this.style.visibility = "hidden";
+  });
+
+  matrixDiv.appendChild(santaImg);
+}
