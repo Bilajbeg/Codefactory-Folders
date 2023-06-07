5093ad29ad736f04fa6fc97056212329fc6caa19
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-07 13:28:52 +0200

    finished

--- javascript_day5/basic/script_1.js
+++ javascript_day5/basic/script_1.js
@@ -6,14 +6,60 @@ power;
     constructor(brand, model, power) {
         this.brand = brand;
         this.model = model;
-        this.speed = power;
+        this.power = power;
     }
 
-    info() {
-    return `This car is a  ${this.brand}  and model  ${this.model} with ${this.power} ps!`
+    car_info() {
+    return `This car is a ${this.brand} and model  ${this.model} with ${this.power} ps!<br><br>`
     }
 }
 
-const car_1= document.getElementById(`cars`).info()
+const car = document.getElementById("cars")
 
-car_1.innerHTML += new cars("Audi","A5", 240);
+car.innerHTML += new cars("Audi", "A6", 300).car_info()
+car.innerHTML += new cars("BMW", "530d", 150).car_info()
+car.innerHTML += new cars("Volkswagen", "Passat", 200).car_info()
+
+class animals {
+    race;
+    location;
+    weight;
+    
+        constructor(race, location, weight) {
+            this.race = race;
+            this.location = location;
+            this.weight = weight;
+        }
+    
+        animal_info() {
+        return `A ${this.race} is living in ${this.location} with  a weight of ${this.weight} kg!<br><br>`
+        }
+    }
+    
+    const animal = document.getElementById("animal")
+    
+    animal.innerHTML += new animals("Lion", "Africa", 190).animal_info()
+    animal.innerHTML += new animals("Tiger", "Asia", 300).animal_info()
+    animal.innerHTML += new animals("Icebear", "North Pole", 450).animal_info()
+
+    class persons {
+        name;
+        location;
+        age;
+        
+            constructor(name, location, age) {
+                this.name = name;
+                this.location = location;
+                this.age = age;
+            }
+        
+            person_info() {
+            return `${this.name} is from ${this.location} and his age is ${this.age} years!<br><br>`
+            }
+        }
+        
+        const person = document.getElementById("person")
+        
+        person.innerHTML += new persons("Heinz Fischer", "Austria", 68).person_info()
+        person.innerHTML += new persons("Arslan Karaman", "Türkiye", 35).person_info()
+        person.innerHTML += new persons("Zikri Malawi", "Malaysia", 18).person_info()
