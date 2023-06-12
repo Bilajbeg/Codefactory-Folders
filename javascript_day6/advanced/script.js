7853c76729f582b44bdcada76d894e564adaad63
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-12 13:56:28 +0200

    File filled with book titles,authors ...

---
+++ javascript_day6/advanced/script.js
@@ -0,0 +1,34 @@
+
+let books = `[{
+    "Title": "The Lord of the Ring",
+    "Author": "John R. R. Tolkien",
+    "Read": "true"
+    
+} , {
+    "Title": "1984 ",
+    "Author": "George Orwell",
+    "Read": "true"
+    
+} , {
+    "Title": "Animal Farm",
+    "Author": "George Orwell",
+    "Read": "true"
+} , {
+    "Title": "The Great Gatsby",
+    "Author": "F. Scott Fitzgerald",
+    "Read": "true"
+} , {
+    "Title": "The Intelligent Investor",
+    Author": "Benjamin Graham",
+    "EmaiRead": "true"
+} , {
+    "Title": "A Brief History of Time",
+    "Author": "Stephen Hawking",
+    "Read": "true"
+} , {
+    "Title": "Long Walk to Freedom",
+    "Author": "Nelson Mandela",
+    "Read": "true"
+} 
+]`
+
