09fa1702f709c8c1c1aa902f49c550ed2cb087f8
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-12 15:33:05 +0200

    finished but need to change style when read is true

--- javascript_day6/advanced/script.js
+++ javascript_day6/advanced/script.js
@@ -2,33 +2,38 @@
 let books = `[{
     "Title": "The Lord of the Ring",
     "Author": "John R. R. Tolkien",
-    "Read": "true"
-    
+    "Read": "true",
+    "image": "./img/reading_bed_book.jpg"
 } , {
     "Title": "1984 ",
     "Author": "George Orwell",
-    "Read": "true"
-    
+    "Read": "true",
+    "image": "./img/meeting.jpg"
 } , {
     "Title": "Animal Farm",
     "Author": "George Orwell",
-    "Read": "true"
+    "Read": "true",
+    "image": "./img/breakfast.jpg"
 } , {
     "Title": "The Great Gatsby",
     "Author": "F. Scott Fitzgerald",
-    "Read": "true"
+    "Read": "true",
+    "image": "./img/firness.jpg"
 } , {
     "Title": "The Intelligent Investor",
     "Author": "Benjamin Graham",
-    "Read": "true"
+    "Read": "true",
+    "image": "./img/dinner.jpg"
 } , {
     "Title": "A Brief History of Time",
     "Author": "Stephen Hawking",
-    "Read": "true"
+    "Read": "true",
+    "image": "./img/shopping.jpg"
 } , {
     "Title": "Long Walk to Freedom",
     "Author": "Nelson Mandela",
-    "Read": "true"
+    "Read": "true",
+    "image": "./img/going_work.jpg"
 } 
 ]`;
 
@@ -39,13 +44,13 @@ let newArr = JSON.parse(books)
 console.log(newArr);
 
 
-for (const x of newArr) {
-    document.getElementById("result").innerHTML += `
-    <img src="img_girl.jpg" alt="The Lord of the Ring" width="200" height="200"> 
+for (let i = 0; i < newArr.length; i++) {
+    document.getElementById("result").innerHTML += ` 
     <div>
-        <p>${x.Title}</p>
-        <p>${x.Author}</p>
+        <p>${newArr[i].Title}</p>
+        <p>${newArr[i].Author}</p>
         <p>Read</p>
+        <img src="${newArr[i].image}" alt="" width="200" height="200">
     </div>
     `;
 }
