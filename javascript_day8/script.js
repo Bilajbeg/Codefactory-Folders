59e09ee285e089cb2f814d3f5d76663d778612d1
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-13 12:05:31 +0200

    object flowers added in javascript file

---
+++ javascript_day8/script.js
@@ -0,0 +1,58 @@
+
+
+const flowers = [
+    {
+        name: "Rose ",
+        image: "",
+        price: 5,
+        qtty: 1
+    },
+    {
+        name: "Lily",
+        image: "",
+        price: 4,
+        qtty: 1
+    },
+    {
+        name: "Tulip",
+        image: "",
+        price: 7,
+        qtty: 1
+    },
+    {
+        name: "Orchid",
+        image: "",
+        price: 10,
+        qtty: 1
+    },
+    {
+        name: "Carnation ",
+        image: "",
+        price: 3,
+        qtty: 1
+    },
+    {
+        name: "Hyacinth",
+        image: "",
+        price: 20,
+        qtty: 1
+    },
+    {
+        name: "Chrysanthemum",
+        image: "",
+        price: 8,
+        qtty: 1
+    },
+    {
+        name: "Freesia",
+        image: "",
+        price: 3.5,
+        qtty: 1
+    },
+    {
+        name: "Gladiolus",
+        image: "",
+        price: 12,
+        qtty: 1
+    },
+];
