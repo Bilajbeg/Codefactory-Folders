b2739f038c2c83471dfe033bc1f0daf59af24ccd
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-14 14:09:41 +0200

    finished exercise_2

--- Typescript_day1/basic/exercise_2/script.js
+++ Typescript_day1/basic/exercise_2/script.js
@@ -1,7 +1,52 @@
 "use strict";
-let fullNameObj = {
-    fName: "Emelin",
-    lName: "Bilajbegovic"
-};
-let fullName = fullNameObj.fName + " " + fullNameObj.lName;
-document.write(fullName);
+let fullNameObj = [{
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }, {
+        fName: "Emelin",
+        lName: "Bilajbegovic"
+    }
+];
+// 1.) First task
+// let fullName = fullNameObj.fName + " " + fullNameObj.lName;
+// document.write(fullName);
+// 2.) Second Task
+// const fullNameArray = Array(10).fill(fullNameObj); // This method fills each slot of the array with the provided value
+//  fullNameArray.forEach((fullNameObj) => {
+//  const fullName = fullNameObj.fName + " " + fullNameObj.lName;
+//  document.write(fullName + "<br>");
+// });
+// 3.) Third Task
+function printLastName() {
+    fullNameObj.forEach(test => {
+        document.write(test.lName + "<br>");
+    });
+}
+// Sets the time when it should apply on the screen 
+setTimeout(() => {
+    printLastName();
+}, 5000);
--- Typescript_day1/basic/exercise_2/script.ts
+++ Typescript_day1/basic/exercise_2/script.ts
@@ -1,7 +1,60 @@
-let fullNameObj = {
+let fullNameObj: { fName: string; lName: string; }[] = [{
+
     fName: "Emelin",
     lName: "Bilajbegovic"
-};
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}, {
+    fName: "Emelin",
+    lName: "Bilajbegovic"
+}
 
-let fullName = fullNameObj.fName + " " + fullNameObj.lName;
-document.write(fullName);
+];
+// 1.) First task
+
+// let fullName = fullNameObj.fName + " " + fullNameObj.lName;
+// document.write(fullName);
+
+// 2.) Second Task
+
+// const fullNameArray = Array(10).fill(fullNameObj); // This method fills each slot of the array with the provided value
+
+//  fullNameArray.forEach((fullNameObj) => {
+//  const fullName = fullNameObj.fName + " " + fullNameObj.lName;
+//  document.write(fullName + "<br>");
+// });
+
+// 3.) Third Task
+
+function printLastName() {
+    fullNameObj.forEach(test => {
+        document.write(test.lName + "<br>")
+    });
+}
+
+// Sets the time when it should apply on the screen 
+setTimeout(() => {
+    printLastName()
+}, 5000);
