8b60884d01931fda65a5fe3ce4c90a25043aba80
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-14 11:50:47 +0200

    finished exercise_1

--- Typescript_day1/basic/script.js
+++ Typescript_day1/basic/script.js
@@ -1,1 +1,7 @@
 "use strict";
+for (let i = 1; i <= 10; i++) {
+    document.write(`<br>Table of numbers: ${i}<br><br>`);
+    for (let j = 1; j <= 10; j++) {
+        document.write(`${i} * ${j} = ${i * j} <br><hr>`);
+    }
+}
