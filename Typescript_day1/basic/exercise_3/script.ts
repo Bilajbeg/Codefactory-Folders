bba3796fcff6b7e657c57ce19e3f51eab51a2af2
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-14 23:35:24 +0200

    update made

---
+++ Typescript_day1/basic/exercise_3/script.ts
@@ -0,0 +1,33 @@
+/* const names: string[] = ["Markus", "Martin", "Tarik", "David"]; */
+
+// Print only the index of the elements
+/* document.write("<h3>Printing index of elements:</h3>");
+for (let i = 0; i < names.length; i++) {
+    document.write(`Index : `, i + "<br>")
+};
+document.write("<br>");
+ */
+
+let names: string[] = ["Alice", "Bob", "Charlie", "David"];
+
+// Print only the index of the elements
+document.write("<h3>Printing index of elements:</h3>");
+names.forEach((_, index) => {
+    document.write(index + "<br>");
+});
+
+document.write("<br>");
+
+// Print only the values
+document.write("<h3>Printing values:</h3>");
+names.forEach((name) => {
+    document.write(name + "<br>");
+});
+
+document.write("<br>");
+
+// Print both index and values
+document.write("<h3>Printing index and values:</h3>");
+names.forEach((name, index) => {
+    document.write(`Index: ${index}, Value: ${name}<br>`);
+});
