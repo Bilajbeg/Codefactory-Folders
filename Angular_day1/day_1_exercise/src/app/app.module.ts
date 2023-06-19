f2ce44130250289ac321b55417ca1ecce1106c97
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-19 13:09:08 +0200

    componets content and footer created

--- Angular_day1/day_1_exercise/src/app/app.module.ts
+++ Angular_day1/day_1_exercise/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
