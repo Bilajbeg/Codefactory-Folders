20d25845c00fbe13b7c5d0a44944592377b093cc
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-19 15:06:12 +0200

    exercise for today finished

--- Angular_day1/day_1_exercise/src/app/app-routing.module.ts
+++ Angular_day1/day_1_exercise/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+
+  path: "", component: ContentComponent
+
+},
+{
+  path: "learn-more", component: AboutUsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
