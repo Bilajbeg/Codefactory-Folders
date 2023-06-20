70d75e01afdf94f84692ebaf3f2b6fe4fbd1eefc
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-20 15:10:08 +0200

    about-us component created

--- Angular_day2/day_2_exercise/src/app/app-routing.module.ts
+++ Angular_day2/day_2_exercise/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "about_us", component: AboutUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular_day2/day_2_exercise/src/app/app.module.ts
+++ Angular_day2/day_2_exercise/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
