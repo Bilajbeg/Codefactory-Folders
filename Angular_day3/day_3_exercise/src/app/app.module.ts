4bbc14ca8473956568163d3df776bb697821dc6b
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-21 09:33:03 +0200

    navbar and product-list components created

--- Angular_day3/day_3_exercise/src/app/app.module.ts
+++ Angular_day3/day_3_exercise/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
