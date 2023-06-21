7450523061e73ec5da63babd04e6873a265157bd
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-21 09:42:02 +0200

    component product-details created

--- Angular_day3/day_3_exercise/src/app/app.module.ts
+++ Angular_day3/day_3_exercise/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
