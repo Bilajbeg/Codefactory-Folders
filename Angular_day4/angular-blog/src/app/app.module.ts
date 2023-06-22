96e2ee43bf897eeb68f4eb51fd2a296ccb7dcfae
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-22 11:33:15 +0200

    now working again after removing the components folder

--- Angular_day4/angular-blog/src/app/app.module.ts
+++ Angular_day4/angular-blog/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { BlogPageComponent } from './components/blog-page/blog-page.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
