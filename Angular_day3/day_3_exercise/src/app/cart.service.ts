38c2b8c290af72df40166234d52af68c3d67be68
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-23 10:09:35 +0200

    update

--- Angular_day3/day_3_exercise/src/app/cart.service.ts
+++ Angular_day3/day_3_exercise/src/app/cart.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Iproducts } from './Iproducts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cart: Iproducts[] = [];
 
   constructor() { }
+
+  addCart(obj: Iproducts) {
+    this.cart.push(obj);
+  }
+  getCart() {
+    return this.cart
+  }
+  clearCart() {
+    this.cart = [];
+    return this.cart;
+  }
+  calculateTotal() {
+    let total: number = 0;
+    for (let val of this.cart) {
+      total = total + val.price;
+    }
+    return total;
+  }
 }
--- Angular_day3/day_3_exercise/src/app/product-details/product-details.component.ts
+++ Angular_day3/day_3_exercise/src/app/product-details/product-details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Iproducts } from '../Iproducts';
+import { ActivatedRoute, Params } from '@angular/router';
+import { products } from '../products';
+import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Iproducts = {} as Iproducts;
+  index: number = 0;
+
+  constructor(private route: ActivatedRoute, private cartService: CartService) {
+
+  }
+
+  addToCart() {
+    alert("your Product Added To The Cart")
+    this.cartService.addCart(this.product)
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.index = +params['indexFormRouting'];
+      this.product = products[this.index];
+      console.log(this.index);
+
+    });
+  }
 
 }
