42696e145232c135bac9601a22323cd268597235
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-08-10 00:07:37 +0200

    images added

--- Back End/Symfony/day_3_test/src/Form/TodoType.php
+++ Back End/Symfony/day_3_test/src/Form/TodoType.php
@@ -6,17 +6,34 @@ use App\Entity\Todo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class TodoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('priority')
-            ->add('createDate')
-        ;
+            ->add('name', null, [
+                'attr' => [
+                    'class' => 'mt-2 form-control',
+                    "placeholder" => "Please type the todo name"
+                ],
+            ])
+            ->add('description', null, [
+                'attr' => ['class' => 'form-control mt-2'],
+            ])
+            ->add('priority', ChoiceType::class, [
+                'attr' => ['class' => 'form-control mt-2'],
+                'choices'  => [
+                    'Low' => 'Low',
+                    'Normal' => 'Normal',
+                    'High' => 'High',
+                ],
+            ])
+            ->add('createDate', null, [
+                'attr' => ['class' => 'form-control mt-2'],
+                "widget" => "single_text"
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
