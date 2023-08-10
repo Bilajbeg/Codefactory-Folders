a70c1a197a499466459e8cb6f179c2ef8ff3e160
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-08-10 10:52:51 +0200

    day_5 Symfony project with LOGIN

---
+++ Back End/Symfony/test_day5/src/Controller/UserAccessController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/user')]
+class UserAccessController extends AbstractController
+{
+    #[Route('/', name: 'app_user_access')]
+    public function index(ProductRepository $productRepo): Response
+    {
+
+        return $this->render('user_access/index.html.twig', [
+            'products' => $productRepo->findAll()
+        ]);
+    }
+}
